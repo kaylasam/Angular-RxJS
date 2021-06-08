15b0f645a748b3fe3b0cab4881df109b3ceab5f9
Kayla Hernandez <kayla.hernandez@jbhunt.com>
2021-06-08 16:08:06 -0500

    filtering products

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { combineLatest, EMPTY, Observable, Subject } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { ProductCategoryService } from '../product-categories/product-category.service';
 
@@ -14,32 +14,33 @@ import { ProductService } from './product.service';
 export class ProductListComponent{
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
-  selectedCategoryId = 1;
+
+  private categorySelectedSubject = new Subject<number>();      //create the action stream (private bc no other code should use this subject)
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();      //expose the subject's observable
 
   //defining categories stream and assigning to categories observable defined in product category service
   categories$ = this.productCategoryService.productCategories$
     .pipe(
         catchError(err => {
             this.errorMessage = err;
-            return EMPTY;   //returns nothing if there is an error
+            return EMPTY;   // returns nothing if there is an error
         })
     );
 
-  products$ = this.productService.productsWithCategory$.pipe(     // instead of product$ observable, this observable is identical except it provides the category property
-    catchError(err => {           //catch and replace
+  products$ = combineLatest([       
+    this.productService.productsWithCategory$,        // combining action stream w data stream
+    this.categorySelectedAction$
+  ])
+    .pipe(     // instead of product$ observable, this observable is identical except it provides the category property
+      map (([products, selectedCategoryId]) =>        // first stream emits array of products, second stream emits the selected category id when user selects from the drop down
+      products.filter(product =>          // filters list of products
+        selectedCategoryId ? product.categoryId === selectedCategoryId : true
+      )),
+    catchError(err => {           // catch and replace
       this.errorMessage = err;
       return EMPTY;               // returns observable that emits empty array
     })
   );
-
-  productSimpleFilter$ = this.productService.productsWithCategory$
-    .pipe (
-      map(products => 
-        products.filter(product =>
-          this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
-          ))
-    );
 
   constructor(private productService: ProductService, private productCategoryService: ProductCategoryService) { }
 
@@ -47,7 +48,7 @@ export class ProductListComponent{
     console.log('Not yet implemented');
   }
 
-  onSelected(categoryId: string): void {
-    this.selectedCategoryId = +categoryId; 
+  onSelected(categoryId: string): void {        // called each time the users selects from the drop down
+    this.categorySelectedSubject.next(+categoryId);     // emits the selected category id to the stream
   }
 }
