f9fd4c202ee376f1528c398132e458ad66b5cc32
Kayla Hernandez <kayla.hernandez@jbhunt.com>
2021-06-16 09:04:40 -0500

    page title reflects the product selected

--- APM/src/app/products/product-list-alt/product-detail.component.ts
+++ APM/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { EMPTY, Subject } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { Product } from '../product';
 
 import { ProductService } from '../product.service';
 
@@ -10,7 +11,6 @@ import { ProductService } from '../product.service';
   changeDetection: ChangeDetectionStrategy.OnPush     // allows view to be updated with product is selected
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
   private errorMessageSubject = new Subject<string>();
   errorMessage$ = this.errorMessageSubject.asObservable();
 
@@ -20,6 +20,12 @@ export class ProductDetailComponent {
         this.errorMessageSubject = err;
         return EMPTY;
       })
+    );
+
+  pageTitle$ = this.product$
+    .pipe(
+      map((p: Product) =>
+      p ? `Product Detail for: ${p.productName}` : null)
     );
 
   productSuppliers$ = this.productService.selectedProductSuppliers$         // gets the supplier for the selected product and assigns to productSuppliers$
